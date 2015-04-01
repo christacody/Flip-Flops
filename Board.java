6a32b64815fbc1aabc76f05172c5272633f3764b
Leon Allen <leon.allen22@gmail.com>
2015-04-01 18:38:57 -0500

    Added action listener to switch between flip-flops

--- Board.java
+++ Board.java
@@ -5,23 +5,79 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import java.awt.BorderLayout;
+import javax.swing.SwingUtilities;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.BorderFactory;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.Color;
 
 public class Board
 {
+	private static final int BOARD_WIDTH = 840;
+	private static final int BOARD_HEIGHT = 800;
+	private final int DELAY = 6;
+
 	public static void main(String[] args)
 	{
-		new Board();
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				createGUI();
+			}
+		});
 	}
 
-	public Board()
+	private static void createGUI()
 	{
 		JFrame frame = new JFrame();
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Computer Architecture");
-		frame. setSize(500, 450);
+		frame.setLocationRelativeTo(null);
+
+		String[] opts = {"SR Latch", "D Flip-Flop", "T Flip-Flop"};
+		final JPanel comboPanel = new JPanel();
+		JLabel comboLbl = new JLabel("Examples: ");
+		final JComboBox<String> examples = new JComboBox<String>(opts);
+
+		comboPanel.add(comboLbl);
+		comboPanel.add(examples);
+
+		final JPanel listPanel = new JPanel();
+		listPanel.setVisible(false);
+
+		frame.add(comboPanel, BorderLayout.NORTH);
+		frame.add(listPanel, BorderLayout.CENTER);
+
+		final MyPanel pnl = new MyPanel();
+
+		frame.add(pnl);
+		frame.pack();
+
+		frame.setVisible(true);
+
+		examples.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				pnl.setMode((String)examples.getSelectedItem());
+			}
+		});
+	}
+
+	/*public Board()
+	{
+		JFrame frame = new JFrame();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Computer Architecture");
+		frame.setSize(BOARD_WIDTH, BOARD_HEIGHT);
 		frame.setLocationRelativeTo(null);
 
 		String[] opts = {"SR Latch", "D Flip-Flop", "T Flip-Flop"};
@@ -39,5 +95,79 @@ public class Board
 		frame.add(listPanel, BorderLayout.CENTER);
 
 		frame.setVisible(true);
+	}*/
+
+	/*public void paint(Graphics g)
+	{
+		super.paint(g);
+
+		g.setColor(Color.black);
+		g.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+
+		Toolkit.getDefaultToolkit().sync();
+		g.dispose();
+	}
+
+	public void cycle()
+    {
+
+    }
+
+	public void run()
+    {
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while(true)
+        {
+            repaint();
+            cycle();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if (sleep < 0)
+                sleep = 2;
+
+            try
+            {
+                Thread.sleep(sleep);
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("interrupted");
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }*/
+}
+
+class MyPanel extends JPanel
+{
+	private String mode = "SR Latch";
+
+	public MyPanel()
+	{
+		setBorder(BorderFactory.createLineBorder(Color.black));
+	}
+
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(800, 840);
+	}
+
+	public void setMode(String newMode)
+	{
+		mode = newMode;
+		super.repaint();
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		g.drawString(mode, 10, 20);
 	}
 }
