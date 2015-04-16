ef0d780b92b0ca1e07d87d69b472a987556eed85
Leon Allen <leon.allen22@gmail.com>
2015-04-16 18:05:35 -0500

    Added timer to periodically repaint diagram; started work on a way to animate circuits when input in received

---
+++ Board.java
@@ -0,0 +1,329 @@
+/**
+ * Display interactive diagrams of circuit components including
+ * S-R Latch, D Flip-Flop, and T Flip-Flop.
+ *
+ * TODO: pulse is currently a boolean array used for animating
+ *       the circuits when input is given. Going to change it
+ *       to utilize bitwise operations instead
+ *
+ * @author	Harrry Allen
+ * @author Christa Cody
+ */
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.geom.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Board
+{
+	private static final int BOARD_WIDTH = 840;
+	private static final int BOARD_HEIGHT = 800;
+	private final int DELAY = 6;
+
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				createGUI();
+			}
+		});
+	}
+
+	/** Initialize GUI */
+	private static void createGUI()
+	{
+		JFrame frame = new JFrame();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Computer Architecture");
+		frame.setLocationRelativeTo(null);
+
+		//Make the drop-down list of diagram options & labels for the options
+		String[] opts = {"SR Latch", "D Flip-Flop", "T Flip-Flop"};
+		final JPanel comboPanel = new JPanel();
+		JLabel comboLbl = new JLabel("Diagrams: ");
+		final JComboBox<String> examples = new JComboBox<String>(opts);
+
+		//Add labels to the drop-down list
+		comboPanel.add(comboLbl);
+		comboPanel.add(examples);
+
+		//Create panel for drop-down list
+		final JPanel listPanel = new JPanel();
+		listPanel.setVisible(false);
+
+		//Add panels to frame
+		frame.add(comboPanel, BorderLayout.NORTH);
+		frame.add(listPanel, BorderLayout.CENTER);
+
+		//pnl is where the diagrams are drawn
+		final MyPanel pnl = new MyPanel();
+
+		//Add button to pnl for input to circuit diagrams
+		JButton but = new JButton(".");
+		but.addActionListener(pnl);
+		but.setActionCommand("A");
+		pnl.add(but);
+
+		//Add pnl to frame and make it visible
+		frame.add(pnl);
+		frame.pack();
+		frame.setVisible(true);
+
+		//Add actionListener to respond to changes in diagram option
+		examples.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				pnl.setMode((String)examples.getSelectedItem());
+			}
+		});
+	}
+
+	/*public Board()
+	{
+		JFrame frame = new JFrame();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Computer Architecture");
+		frame.setSize(BOARD_WIDTH, BOARD_HEIGHT);
+		frame.setLocationRelativeTo(null);
+
+		String[] opts = {"SR Latch", "D Flip-Flop", "T Flip-Flop"};
+		final JPanel comboPanel = new JPanel();
+		JLabel comboLbl = new JLabel("Examples: ");
+		JComboBox<String> examples = new JComboBox<String>(opts);
+
+		comboPanel.add(comboLbl);
+		comboPanel.add(examples);
+
+		final JPanel listPanel = new JPanel();
+		listPanel.setVisible(false);
+
+		frame.add(comboPanel, BorderLayout.NORTH);
+		frame.add(listPanel, BorderLayout.CENTER);
+
+		frame.setVisible(true);
+	}*/
+
+	/*public void paint(Graphics g)
+	{
+		super.paint(g);
+
+		g.setColor(Color.black);
+		g.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+
+		Toolkit.getDefaultToolkit().sync();
+		g.dispose();
+	}
+
+	public void cycle()
+    {
+
+    }
+
+	public void run()
+    {
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while(true)
+        {
+            repaint();
+            cycle();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if (sleep < 0)
+                sleep = 2;
+
+            try
+            {
+                Thread.sleep(sleep);
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("interrupted");
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }*/
+}
+
+class MyPanel extends JPanel implements ActionListener
+{
+	private String mode = "SR Latch";	//Determines which diagram is shown
+	private int andWidth = 50;			//Width of the AND/NAND gates
+	private int andHeight = 100;		//Height of the AND/NAND gates
+	private int lineThickness = 4;		//Thickness of lines drawn
+	private boolean[] pulse;			//Indicates pulse to be drawn on diagram
+	private int delay = 600;			//Determines how often diagram is repainted
+	private BasicStroke defaultStroke;	//Default line thickness
+
+	/** Constructor: Set border color to black */
+	public MyPanel()
+	{
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		defaultStroke = new BasicStroke(lineThickness);
+		pulse = new boolean[2];
+
+		// Repaint in increments specified by delay
+		ActionListener drawDelay = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				repaint();
+			}
+		};
+
+		new Timer(delay, drawDelay).start();
+	}
+
+	/** Returns preferred size */
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(800, 840);
+	}
+
+	/** Switch between diagrams */
+	public void setMode(String newMode)
+	{
+		mode = newMode;
+		super.repaint();
+	}
+
+	/** Paint diagram to JPanel */
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setStroke(new BasicStroke(lineThickness));
+		super.paintComponent(g);
+
+		switch(mode)
+		{
+			//Draw SR Latch diagram
+			case "SR Latch":
+				if(pulse[0])
+				{
+					drawNand(g2, 250, 200);
+					drawNand(g2, 250, 500);
+					g2.setColor(Color.red);
+					g2.drawLine(250-50, 200+20, 246, 200+20);
+					g2.setColor(Color.black);
+					pulse[0] = false;
+					pulse[1] = true;
+				}
+
+				else if(pulse[1])
+				{
+					drawNand(g2, 250, 200);
+					drawNand(g2, 250, 500);
+					g2.setColor(Color.green);
+					g2.drawLine(250-50, 200+20, 246, 200+20);
+					g2.setColor(Color.black);
+					pulse[1] = false;
+				}
+
+				else
+				{
+					drawNand(g2, 250, 200);
+					drawNand(g2, 250, 500);
+					g2.drawLine(250-50, 200+20, 246, 200+20);
+				}
+			break;
+
+			default:
+				g.drawString(mode, 10, 20);
+		}
+	}
+
+	/** Draw an AND gate at coordinates (x, y) */
+	public void drawAnd(Graphics2D g2, int x, int y)
+	{
+		g2.drawRect(x, y, andWidth, andHeight);
+		g2.clearRect(x+(andWidth-(lineThickness/2)), y+1, lineThickness, andHeight-1);
+		g2.drawArc(x+(andWidth-50), y, 100, andHeight, 270, 180);
+	}
+
+	/** Draw a NAND gate at coordinates (x, y) */
+	public void drawNand(Graphics2D g2, int x, int y)
+	{
+		drawAnd(g2, x, y);
+		drawNot(g2, x+(andWidth+50), y+(andHeight/2-7));
+	}
+
+	/** Draw an OR gate at coordinates (x, y) */
+	public void drawOr(Graphics2D g2, int x, int y)
+	{
+		g2.drawArc(x, y, 30, 100, 270, 180);
+		g2.drawArc(x, y, 150, 50, 0, 60);
+	}
+
+	/** Draw a NOR gate at coordinates (x, y) */
+	public void drawNor(Graphics2D g2, int x, int y)
+	{
+
+	}
+
+	/** Draw a NOT at coordinates (x,y) */
+	public void drawNot(Graphics2D g2, int x, int y)
+	{
+		g2.drawOval(x, y, 12, 12);
+	}
+
+	/** Draw a point of I/O at coordinates (x,y) */
+	public void drawIOPoint(Graphics2D g2, int x, int y)
+	{
+		g2.setStroke(new BasicStroke(2));
+		g2.drawOval(x, y, 7, 7);
+		g2.setStroke(defaultStroke);
+	}
+
+	/** Draw a connection represented as a filled dot
+	 *	between circuits at coordinates (x,y)
+	 */
+	public void drawConnect(Graphics2D g2, int x, int y)
+	{
+		g2.fillOval(x, y, 8, 8);
+	}
+
+	public void sendPulse()
+	{
+		if(pulse[0])
+		{
+			pulse[0] = false;
+			pulse[1] = true;
+		}
+		else if(pulse[1])
+		{
+			pulse[1] = false;
+		}
+		else
+		{
+			pulse[0] = true;
+		}
+	}
+
+	/** Respond to ActionEvents generated by buttons in JPanel */
+	public void actionPerformed(ActionEvent e)
+	{
+		String command = e.getActionCommand();
+
+		switch(command)
+		{
+			case "A":
+				//Do stuff
+				sendPulse();
+			break;
+		}
+	}
+}
