875641bce725c6b89d59fe09913dc0941aaca97b
Leon Allen <leon.allen22@gmail.com>
2015-04-20 18:27:44 -0500

    Cleaned up some code

--- Board.java
+++ Board.java
@@ -2,17 +2,9 @@
  * Display interactive diagrams of circuit components including
  * S-R Latch, D Flip-Flop, and T Flip-Flop.
  *
- * TODO: - pulse is currently a boolean array used for animating
- *         the circuits when input is given. Going to change it
- *         to utilize bitwise operations instead
- *		 - Finish drawOr & drawNor functions
- *		 - Finish S-R Latch
- *		 - Animate S-R Latch
- *		 - Design D Flip-Flop
- *		 - Animate D Flip-Flop
- * 		 - Design T Flip-Flop
- *		 - Animate T Flip-Flop
+ * TODO:
  *		 - Possibly clean up graphics
+		 - Add JK Flip-Flop
  *
  * @author	Harrry Allen
  * @author	Christa Cody
@@ -605,46 +597,6 @@ class MyPanel extends JPanel implements ActionListener, KeyListener
 		}
 	}
 
-
- /*   public void drawTFlipFlop(Graphics2D g2)
-	{
-		int xXOR = 250;
-		int yXOR = 200;
-		Font f = new Font("Monospaced", 1, 26);
-		g2.setFont(f);
-		g2.drawString("T", xXOR-100, yXOR+53);
-		g2.drawString("Clk", xXOR-125, yXOR+205);
-		g2.drawString("D", xXOR+205, yXOR+35);
-		g2.drawString("Clk", xXOR+205, yXOR+205);
-		g2.drawString("Q", xXOR+380, yXOR+35);
-		g2.drawLine(xXOR+383, yXOR+187, xXOR+391, yXOR+187);
-		g2.drawString("Q", xXOR+380, yXOR+205);
-		g2.drawString("Q", xXOR+605, yXOR+35);
-		g2.drawLine(xXOR+608, yXOR+182, xXOR+616, yXOR+182);
-		g2.drawString("Q", xXOR+605, yXOR+200);
-		g2.drawRect(xXOR+200, yXOR-20, 200, 300);
-		drawSegT1(g2, xXOR, yXOR);
-		drawSegT2(g2, xXOR, yXOR);
-		drawSegT3(g2, xXOR, yXOR);
-		drawSegT4(g2, xXOR, yXOR);
-		drawSegT5(g2, xXOR, yXOR);
-		drawSegT6(g2, xXOR, yXOR);
-		drawSegT7(g2, xXOR, yXOR);
-		drawSegT8(g2, xXOR, yXOR);
-		drawSegT9(g2, xXOR, yXOR);
-		drawSegT10(g2, xXOR, yXOR);
-		drawXOR(g2, xXOR, yXOR);
-	}
-
-	public void drawTFFanimation(Graphics2D g2, int x, int y)
-	{
-
-		    g2.setColor(Color.green);
-			drawSeg13(g2, tAndX, bAndY);
-			drawSeg14(g2, tAndX, bAndY);
-			g2.setColor(Color.black);
-
-	}*/
 
 	/** Draw an AND gate at coordinates (x, y) */
 	public void drawAnd(Graphics2D g2, int x, int y)
@@ -1265,47 +1217,6 @@ class MyPanel extends JPanel implements ActionListener, KeyListener
 	{
 		g2.drawLine(tAndX-62, tAndY+150, tAndX-62, bAndY+46);
 	}
-
-/*	private void drawSegT1(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX-70, tAndY+46, tAndX+30, tAndY+46);
-	}
-	private void drawSegT2(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX-62, tAndY+15, tAndX+27, tAndY+15);
-	}
-	private void drawSegT3(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX-62, tAndY-100, tAndX-62, tAndY+15);
-	}
-	private void drawSegT4(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX-62, tAndY-100, tAndX+500, tAndY-100);
-	}
-	private void drawSegT5(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX+500, tAndY-100, tAndX+500, tAndY+35);
-	}
-	private void drawSegT6(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX+400, tAndY+35, tAndX+500, tAndY+35);
-	}
-	private void drawSegT7(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX+500, tAndY+35, tAndX+600, tAndY+35);
-	}
-	private void drawSegT8(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX-70, tAndY+200, tAndX+200, tAndY+200);
-	}
-	private void drawSegT9(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX+400, tAndY+200, tAndX+600, tAndY+200);
-	}
-	private void drawSegT10(Graphics2D g2, int tAndX, int tAndY)
-	{
-		g2.drawLine(tAndX+120, tAndY+35, tAndX+200, tAndY+35);
-	}*/
 
 	/**
 	 * If input is received, set pulse to integer & bit shift by 1 upon
